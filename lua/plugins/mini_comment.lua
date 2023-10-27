820811dcb9254b5745b8449da920a13ed3daff5e
deng232 <78670850+deng232@users.noreply.github.com>
2023-10-27 01:32:11 -0400

    test

---
+++ lua/plugins/mini_comment.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "echasnovski/mini.comment",
+    opts = {
+      mappings = {
+        comment = "gc",
+        comment_line = "gl",
+        textobject = "gc",
+      },
+    },
+  },
+}
